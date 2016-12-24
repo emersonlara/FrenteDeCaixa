744ac080eb2df9e491f6b3af12dab6f2267784dd
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2016-12-23 23:14:16 -0200

    Adicionado | VendaApplication
    VendaApplication Implementado

---
+++ FrenteDeCaixa/Application/VendaApplication.cs
@@ -0,0 +1,50 @@
+﻿using FrenteDeCaixa.Context;
+using FrenteDeCaixa.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenteDeCaixa.Application
+{
+    class VendaApplication
+    {
+        private EntidadesContext Banco { get; set; }
+
+        public VendaApplication()
+        {
+            Banco = new EntidadesContext();
+        }
+
+        public void Salvar(Venda venda)
+        {
+            Banco.Vendas.Add(venda);
+            Banco.SaveChanges();
+        }
+
+        public void Alterar(Venda venda)
+        {
+            Venda vendaAux = Banco.Vendas
+                .Where(x => x.Id == venda.Id).First();
+            vendaAux.UsuarioId = venda.UsuarioId;
+            vendaAux.Usuario = venda.Usuario;
+            vendaAux.ClienteId = venda.ClienteId;
+            vendaAux.Cliente = venda.Cliente;
+            vendaAux.FormaDePagamentoId = venda.FormaDePagamentoId;
+            vendaAux.FormaDePagamento = venda.FormaDePagamento;
+            vendaAux.ValorTotal = venda.ValorTotal;
+        }
+
+        public void Excluir(Venda venda)
+        {
+            Banco.Set<Venda>().Remove(venda);
+            Banco.SaveChanges();
+        }
+
+        public IEnumerable<Venda> Listar()
+        {
+            return from c in Banco.Vendas select c;
+        }
+    }
+}
