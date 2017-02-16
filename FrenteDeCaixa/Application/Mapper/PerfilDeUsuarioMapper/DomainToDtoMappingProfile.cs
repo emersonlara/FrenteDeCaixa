ecf7523280e96a81dc4e6c5d402302fa64f91331
JFRode <joaofelipe.rode@gmail.com>
2017-02-15 22:46:10 -0200

    Adicionado | Mappers Usuario,Clientes, Produto,FormaDePagamento e Vendas
    Adicionado Mappers Usuario, Clientes, Produto, FormaDePagamento e Vendas

--- FrenteDeCaixa/Application/Mapper/PerfilDeUsuarioMapper/DomainToDtoMappingProfile.cs
+++ FrenteDeCaixa/Application/Mapper/PerfilDeUsuarioMapper/DomainToDtoMappingProfile.cs
@@ -1,6 +1,16 @@
 ﻿using AutoMapper;
+using FrenteDeCaixa.Application.Service.Cliente.Dto;
+using FrenteDeCaixa.Application.Service.FormaDePagamento.Dto;
 using FrenteDeCaixa.Application.Service.PerfilDeUsuario.Dto;
+using FrenteDeCaixa.Application.Service.Produto.Dto;
+using FrenteDeCaixa.Application.Service.Usuario.Dto;
+using FrenteDeCaixa.Application.Service.Venda.Dto;
+using FrenteDeCaixa.Domain.Cliente;
+using FrenteDeCaixa.Domain.FormaDePagamento;
 using FrenteDeCaixa.Domain.PerfilDeUsuario;
+using FrenteDeCaixa.Domain.Produto;
+using FrenteDeCaixa.Domain.Usuario;
+using FrenteDeCaixa.Domain.Venda;
 
 namespace FrenteDeCaixa.Application.Mapper.PerfilDeUsuarioMapper
 {
@@ -9,6 +19,11 @@ namespace FrenteDeCaixa.Application.Mapper.PerfilDeUsuarioMapper
         public DomainToDtoMappingProfile()
         {
             CreateMap<PerfilDeUsuarioDomain, PerfilDeUsuarioDto>();
+            CreateMap<UsuarioDomain, UsuarioDto>();
+            CreateMap<ClienteDomain, ClienteDto>();
+            CreateMap<ProdutoDomain, ProdutoDto>();
+            CreateMap<FormaDePagamentoDomain, FormaDePagamentoDto>();
+            CreateMap<VendaDomain, VendaDto>();
         }
     }
 }
--- FrenteDeCaixa/Application/Mapper/PerfilDeUsuarioMapper/DtoToDomainMappingProfile.cs
+++ FrenteDeCaixa/Application/Mapper/PerfilDeUsuarioMapper/DtoToDomainMappingProfile.cs
@@ -1,6 +1,16 @@
 ﻿using AutoMapper;
+using FrenteDeCaixa.Application.Service.Cliente.Dto;
+using FrenteDeCaixa.Application.Service.FormaDePagamento.Dto;
 using FrenteDeCaixa.Application.Service.PerfilDeUsuario.Dto;
+using FrenteDeCaixa.Application.Service.Produto.Dto;
+using FrenteDeCaixa.Application.Service.Usuario.Dto;
+using FrenteDeCaixa.Application.Service.Venda.Dto;
+using FrenteDeCaixa.Domain.Cliente;
+using FrenteDeCaixa.Domain.FormaDePagamento;
 using FrenteDeCaixa.Domain.PerfilDeUsuario;
+using FrenteDeCaixa.Domain.Produto;
+using FrenteDeCaixa.Domain.Usuario;
+using FrenteDeCaixa.Domain.Venda;
 
 namespace FrenteDeCaixa.Application.Mapper.PerfilDeUsuarioMapper
 {
@@ -9,6 +19,11 @@ namespace FrenteDeCaixa.Application.Mapper.PerfilDeUsuarioMapper
         public DtoToDomainMappingProfile()
         {
             CreateMap<PerfilDeUsuarioDto, PerfilDeUsuarioDomain>();
+            CreateMap<UsuarioDto, UsuarioDomain>();
+            CreateMap<ClienteDto, ClienteDomain>();
+            CreateMap<ProdutoDto, ProdutoDomain>();
+            CreateMap<FormaDePagamentoDto, FormaDePagamentoDomain>();
+            CreateMap<VendaDto, VendaDomain>();
         }
     }
 }
